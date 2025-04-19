f2bb5091e40bf451c6e51329d37bbf3aa19f62e6
Avinash <avinashnarwariya5@gmail.com>
2025-04-19 13:31:14 +0530

    deploying issues resolved

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -4,11 +4,8 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { User } from "next-auth";
 
-export async function DELETE(
-  request: Request,
-  params: { params: { messageid: string } }
-) {
-  const messageId = params.params.messageid;
+export async function DELETE(request: Request, params: { messageid: string }) {
+  const messageId = params.messageid;
   await dbConnect();
   const session = await getServerSession(authOptions);
   const user: User = session?.user;
