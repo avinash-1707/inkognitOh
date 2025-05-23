2be286647c7761cae4f1f864ed59bf241742325a
Avinash <avinashnarwariya5@gmail.com>
2025-05-23 21:10:18 +0530

    modified message suggestion prompt

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -9,7 +9,7 @@ export const maxDuration = 30;
 
 export async function POST(request: Request) {
   const prompt =
-    "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Focus on funny and sarcastic messages that engage people emotionally. For example, your output should be structured like this: What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment. Also each message should be no more than 50 characters";
+    "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me/NGL, and should be suitable for a diverse audience. Focus on funny, sarcastic, and bluntly honest messages that engage people emotionally. For example, your output should be structured like this: What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?. Ensure the questions are intriguing, slightly provocative, foster curiosity, and lean towards a playfully edgy conversational environment. Also each message should be no more than 50 characters.";
 
   try {
     const response = await ai.models.generateContent({
