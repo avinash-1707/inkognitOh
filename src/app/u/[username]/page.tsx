1409e352d44c50878047160366744edf07d9e196
Avinash <avinashnarwariya5@gmail.com>
2025-07-22 12:24:13 +0530

    improved public message page

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -88,9 +88,9 @@ const SendMessage = () => {
   };
 
   return (
-    <div className="pt-14 min-h-screen bg-gradient-to-b from-gray-900 via-black to-gray-900">
-      <div className="container mx-auto my-8 p-6 bg-white/10 bg-opacity-20 backdrop-blur-sm rounded-xl max-w-4xl text-center">
-        <h1 className="text-4xl font-bold mb-6 text-center text-white">
+    <div className="pt-14 min-h-screen">
+      <div className="container mx-auto my-8 p-6 bg-black/10 dark:bg-white/10 bg-opacity-20 backdrop-blur-sm rounded-md max-w-4xl text-center">
+        <h1 className="text-4xl font-bold mb-8 text-center">
           Public Profile Link
         </h1>
         <Form {...form}>
@@ -100,11 +100,13 @@ const SendMessage = () => {
               name="content"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="text-white/50">Your Message</FormLabel>
+                  <FormLabel className="dark:text-white/50 text-black/50">
+                    Send anonymous message to @{params.username}
+                  </FormLabel>
                   <FormControl>
                     <Textarea
                       placeholder="Drop your anonymous message here ..."
-                      className="resize-none border-none bg-white/30 bg-opacity-20 backdrop-blur-sm rounded-xl text-white"
+                      className="resize-none border-none bg-white/30 bg-opacity-20 backdrop-blur-sm rounded-md focus-visible:ring-0"
                       {...field}
                     />
                   </FormControl>
@@ -123,33 +125,35 @@ const SendMessage = () => {
                 </Button>
               </>
             ) : (
-              <Button className="bg-blue-600/40" type="submit">
-                Send it!
-              </Button>
+              <Button type="submit">Send it!</Button>
             )}
           </form>
         </Form>
       </div>
-      <div className="container mx-auto my-8 p-6 bg-white/10 bg-opacity-20 backdrop-blur-sm rounded-xl rounded max-w-4xl">
+      <div className="container mx-auto my-8 p-6 max-w-4xl">
         <h1 className="text-2xl font-bold mb-4 text-center text-white">
           Get AI suggested messages
         </h1>
         <div className="space-y-2">
-          <Button onClick={fetchAiMessages} className="my-4 bg-blue-600/40">
+          <Button
+            variant="outline"
+            onClick={fetchAiMessages}
+            className="my-4 shadow-md"
+          >
             {isSuggestionLoading ? (
               <Loader2 className="mr-2 h-4 w-4 animate-spin" />
             ) : (
               "Suggest messages"
             )}
           </Button>
-          <p className="text-white/60">
+          <p className="text-gray-500">
             Click on any message below to select it.
           </p>
         </div>
         <div className="mt-4 grid grid-cols-1 md:grid-cols-1 gap-6">
           {aiSuggestions.map((suggestion, index) => (
             <Button
-              className="mb-2 bg-gray-800 text-white/80 border-none hover:bg-gray-900 hover:text-white"
+              className="mb-2 bg-gray-300 hover:bg-gray-400/70 dark:bg-gray-800 text-black/80 dark:text-white/80 border-none dark:hover:bg-gray-900"
               variant="outline"
               key={index}
               onClick={() => handleMessageClick(suggestion)}
@@ -161,13 +165,13 @@ const SendMessage = () => {
       </div>
       <Separator className="my-6" />
       <div className="text-center">
-        <div className="mb-4 text-2xl text-white">Get Your Message Board</div>
+        <div className="mb-4 text-2xl">Get Your Message Board</div>
         <Link href={"/sign-up"}>
           <Button className="mb-8">Create Your Account</Button>
         </Link>
       </div>
-      <footer className="py-8 px-4 text-center text-gray-500 bg-black bottom-0 relative text-sm w-full">
-        <p>© 2025 inkognitOh! — The Home of Anonymous Messaging</p>
+      <footer className="py-8 px-4 text-center text-gray-500 dark:bg-black bg-white bottom-0 relative text-sm w-full">
+        <p>Created and designed by @avinash</p>
       </footer>
     </div>
   );
